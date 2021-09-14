049446e06b53b66afc1ed0dcc2d9b3155fa8de2e
RJasonClarke <70181497+RJasonClarke@users.noreply.github.com>
2021-09-14 13:55:10 -0500

    models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@
 art = Category.create(name: "Art")
 tech = Category.create(name: "Tech")
 science = Category.create(name: "Science")
+
+Syllabus.create(title:"My ass", description:"Wide and round.", image_url: "https://daphnechicagodotcom.files.wordpress.com/2020/11/kingcobra-jack.jpg?w=1280", category_id: art_id)
